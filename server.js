9b1f2627b8adf64ca0b9f387e917f041af05f75c
sukh715 <sukhmansingh715@gmail.com>
2023-10-12 00:59:54 +0530

    added files

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const users = {}
+
+const PORT = process.env.PORT || 3000
+
+
+http.listen(PORT, () =>{
+    console.log(`Listening on port ${PORT}`)
+})
+
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res) =>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+
+    socket.on('new-user-joined', name =>{
+        console.log("New User", name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined',name);
+    })
+
+
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+
+    
+
+});
+
+
+
